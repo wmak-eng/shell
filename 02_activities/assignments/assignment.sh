c13fdadb8e393834cac1e608fe0958cc3bbe08c2
Whitney Mak <whitneymak89@gmail.com>
2024-11-24 11:33:45 -0500

    Update script commands to copy contents rather than just the directory over

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,7 +28,7 @@ mkdir data
 cd data
 mkdir raw
 cd ..
-mv rawdata data/raw
+mv rawdata/* data/raw
 
 # 3. List the contents of the ./data/raw directory
 cd data/raw
@@ -44,14 +44,14 @@ mkdir event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cp -i raw/rawdata/*server*.log processed/server_logs
+cp -i raw/*server*.log processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp -i raw/rawdata/*user*.log processed/user_logs
-cp -i raw/rawdata/*event*.log processed/event_logs
+cp -i raw/*user*.log processed/user_logs
+cp -i raw/*event*.log processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm raw/rawdata/*ipaddr*
+rm raw/*ipaddr*
 rm processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
